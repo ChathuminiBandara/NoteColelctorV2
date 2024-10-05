0bb9e8b74d88bc7463e3fa7e4baeab508f756e0b
ChathuminiBandara <chathubandara500@gmail.com>
2024-10-05 12:15:38 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/NoteColelctorV2/NoteColelctorV2Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.NoteColelctorV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteColelctorV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteColelctorV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/NoteColelctorV2/NoteColelctorV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.NoteColelctorV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteColelctorV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
